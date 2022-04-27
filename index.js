2db1ce4001b754c7b89e6a5c2f36253007ce9be4
Jamal Ali <jamalali@Jamals-MacBook-Pro.local>
2022-04-27 03:25:38 +0100

    Shopify buy SDK added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert("Hello, World!");
+import 'dotenv/config';
+import Client from 'shopify-buy/index.unoptimized.umd';
+
+// Initializing a client to return content in the store's primary language
+const client = Client.buildClient({
+    domain: process.env.SHOP_URL,
+    storefrontAccessToken: process.env.STOREFRONT_ACCESS_TOKEN
+});
+
+// Fetch all collections, including their products
+client.collection.fetchAllWithProducts().then((collections) => {
+    // Do something with the collections
+    console.log(collections);
+    console.log(collections[0].products);
+});
